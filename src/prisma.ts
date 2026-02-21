f1ed4017ee57eae73c6678d8cbc5d0cdad979030
fairuuz <justcall1313@gmail.com>
2026-02-21 21:12:22 +0700

    chore: save progress backend lesson sebelum cek branch otp

--- src/prisma.ts
+++ src/prisma.ts
@@ -1,6 +1,34 @@
-// src/prisma.ts
+// 1. WAJIB DI PALING ATAS: Paksa baca file .env sebelum yang lain jalan
+import 'dotenv/config'; 
+
+import { Pool } from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from './generated/prisma';
 
-const prisma = new PrismaClient();
+// 2. Ambil URL dari environment
+const connectionString = process.env.DATABASE_URL;
+
+// Debugging: Biar kita yakin URL-nya nggak undefined lagi
+console.log("🔌 URL DB yang dibaca:", connectionString ? "BERHASIL KEBACA" : "KOSONG/UNDEFINED!");
+
+// Cegah aplikasi jalan kalau URL-nya kosong
+if (!connectionString) {
+    throw new Error("DATABASE_URL tidak ditemukan! Cek file .env kamu.");
+}
+
+// 3. Setup Pool dengan SSL (Wajib untuk db.prisma.io)
+const pool = new Pool({ 
+    connectionString,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
+const adapter = new PrismaPg(pool);
+
+const prisma = new PrismaClient({ 
+    adapter, 
+    log: ['query', 'info', 'warn', 'error'] 
+});
 
 export default prisma;
